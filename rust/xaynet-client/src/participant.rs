b502c0fce909067ddf6ba49a7203fb54fdad1730
finiteprods <kwok.doc@gmail.com>
2020-09-17 07:11:37 +0200

    MaskOne struct; MaskObject wrapper; masking logic

--- rust/xaynet-client/src/participant.rs
+++ rust/xaynet-client/src/participant.rs
@@ -5,12 +5,12 @@
 //! [client module]: ../index.html
 
 use std::default::Default;
-
+// TODO may remove MaskMany later
 use xaynet_core::{
     crypto::{ByteObject, EncryptKeyPair, SigningKeyPair},
     mask::{
-        Aggregation, BoundType, DataType, GroupType, MaskConfig, MaskMany, MaskSeed, Masker, Model,
-        ModelType,
+        Aggregation, BoundType, DataType, GroupType, MaskConfig, MaskMany, MaskObject, MaskSeed,
+        Masker, Model, ModelType,
     },
     message::{Message, Sum, Sum2, Update},
     CoordinatorPublicKey, InitError, LocalSeedDict, ParticipantPublicKey, ParticipantSecretKey,
@@ -128,7 +128,7 @@ impl Participant {
         scalar: f64,
         local_model: Model,
     ) -> Message {
-        let (mask_seed, masked_model, masked_scalar) = Self::mask_model(scalar, local_model);
+        let (mask_seed, mask_object) = Self::mask_model(scalar, local_model);
         let local_seed_dict = Self::create_local_seed_dict(sum_dict, &mask_seed);
 
         Message {
@@ -138,8 +138,8 @@ impl Participant {
             payload: Update {
                 sum_signature: self.sum_signature,
                 update_signature: self.update_signature,
-                masked_model,
-                masked_scalar,
+                masked_model: mask_object.vector, // TODO refactor ...
+                masked_scalar: mask_object.scalar.into(), // TODO ... update message
                 local_seed_dict,
             }
             .into(),
@@ -191,9 +191,10 @@ impl Participant {
     }
 
     /// Generate a mask seed and mask a local model.
-    fn mask_model(scalar: f64, local_model: Model) -> (MaskSeed, MaskMany, MaskMany) {
+    fn mask_model(scalar: f64, local_model: Model) -> (MaskSeed, MaskObject) {
         // TODO: use proper config
-        Masker::new(dummy_config()).mask(scalar, local_model)
+        let config = dummy_config();
+        Masker::new(config, config).mask(scalar, local_model) // HACK reuse model mask config
     }
 
     // Create a local seed dictionary from a sum dictionary.
